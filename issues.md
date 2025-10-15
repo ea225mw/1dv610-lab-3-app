0f96c64e3a509fd47d4ed372a8535de2b640d407
Emanuel Andersen <ea225mw@student.lnu.se>
2025-10-15 11:46:48 +0200

    Refactoring: add prepareTableText(). (Forget colorizition for now.)

---
+++ issues.md
@@ -0,0 +1,3 @@
+# Issues with the module
+
+- Need to also split words at these characters: @
--- js/index.js
+++ js/index.js
@@ -1,55 +1,73 @@
-
 import { stringWorks } from 'https://cdn.jsdelivr.net/npm/string-works@0.1.2/+esm'
 
 const sw = stringWorks
 
+// ---------- DOM References ----------
 const textArea = document.querySelector('textarea')
 const longestWordsTD = document.querySelector('#longestWordsTD')
 const longestLettersTD = document.querySelector('#longestLettersTD')
 const shortestWordsTD = document.querySelector('#shortestWordsTD')
 const shortestLettersTD = document.querySelector('#shortestLettersTD')
-let textProvidedByUser
 const mostFrequentLetterTD = document.querySelector('#mostFrequentLetterTD')
 const mostFrequentLetterAmountTD = document.querySelector('#mostFrequentLetterAmountTD')
+const editArea = document.querySelector('#editArea')
 
+let cleanedTextToAnalyze = ''
+let shortestWordObject
+let longestWordObject
 
-textArea.addEventListener('input', () => {
-  textProvidedByUser = textArea.value
-  if (textProvidedByUser === '') {
+editArea.addEventListener('input', () => {
+  cleanedTextToAnalyze = removeHtmlAndKeepPureText()
+
+  if (cleanedTextToAnalyze === '' || editArea.textContent === '') {
     resetTable()
+    editArea.innerHTML = ''
   }
+
   findShortestWord()
   findLongestWord()
   findMostFrequentLetter()
 })
 
+function removeHtmlAndKeepPureText() {
+  const textWithHtmlTags = editArea.innerHTML
+  return textWithHtmlTags
+    .replace(/<div><br><\/div>/g, '\n')
+    .replace(/<div>/g, '\n')
+    .replace(/<br\s*\/?>/g, '\n')
+    .replace(/<\/div>/g, '')
+    .replace(/<[^>]+>/g, '')
+    .replace(/&nbsp/g, '')
+}
+
 function findShortestWord() {
-  const shortestWord = sw.findShortestWord(textProvidedByUser)
-  let newString = ''
-  for (const word of shortestWord.words) {
-    newString += word + ', '
-  }
-  shortestWordsTD.textContent = newString.slice(0, -2)
-  if (shortestWord.numberOfLetters > 0) {
-    shortestLettersTD.textContent = shortestWord.numberOfLetters
+  shortestWordObject = sw.findShortestWord(cleanedTextToAnalyze)
+  shortestWordsTD.textContent = prepareTableText(shortestWordObject.words)
+
+  if (shortestWordObject.numberOfLetters > 0) {
+    shortestLettersTD.textContent = shortestWordObject.numberOfLetters
   }
 }
 
 function findLongestWord() {
-  const longestWord = sw.findLongestWord(textProvidedByUser)
-  let newString = ''
-  for (const word of longestWord.words) {
-    newString += word + ', '
-  }
-  longestWordsTD.textContent = newString.slice(0, -2)
-  if (longestWord.numberOfLetters > 0) {
-    longestLettersTD.textContent = longestWord.numberOfLetters
-  }
+  longestWordObject = sw.findLongestWord(cleanedTextToAnalyze)
+  longestWordsTD.textContent = prepareTableText(longestWordObject.words)
 
+  if (longestWordObject.numberOfLetters > 0) {
+    longestLettersTD.textContent = longestWordObject.numberOfLetters
+  }
+}
+
+function prepareTableText(array) {
+  let concatinatedString = ''
+  for (const word of array) {
+    concatinatedString += word + ', '
+  }
+  return concatinatedString.slice(0, -2)
 }
 
 function findMostFrequentLetter() {
-  const mostFrequentLetter = sw.mostFrequentLetter(textProvidedByUser)
+  const mostFrequentLetter = sw.mostFrequentLetter(cleanedTextToAnalyze)
   if (mostFrequentLetter.length > 0) {
     for (const [key, value] of Object.entries(mostFrequentLetter[0])) {
       mostFrequentLetterTD.textContent = key
@@ -60,8 +78,29 @@ function findMostFrequentLetter() {
 
 function resetTable() {
   const allDataHolders = document.querySelectorAll('td.dataholder')
-  allDataHolders.forEach((td) => td.textContent = '')
+  allDataHolders.forEach((dataholder) => dataholder.textContent = '')
 }
+
+/*
+function colorizeLongestWord() {
+  const treeWalker = document.createTreeWalker(editArea, NodeFilter.SHOW_TEXT)
+  const longestWordRegExp = new RegExp(`\\b(${longestWordObject.words[0]}) \\b`, "gi")
+  console.log(longestWordRegExp);
+
+  const textNodes = []
+  while (treeWalker.nextNode()) {
+    textNodes.push(treeWalker.currentNode)
+  }
+
+  for (const node of textNodes) {
+    if (longestWordRegExp.test(node.textContent)) {
+      const span = document.createElement('span')
+      span.innerHTML = node.textContent.replace(longestWordRegExp, `<span class="longestWord">$1</span>`)
+      node.replaceWith(...span.childNodes)
+    }
+  }
+}
+*/
 
 /**
  * Formattering av text
